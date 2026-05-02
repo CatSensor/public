81355c7bd4307e59f991e04fb94932e932a1899e
William Beaudin <williambeaudun@gmail.com>
2026-05-01 23:22:58 -0400

    Remove rollup-plugin-visualizer from dependencies and configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-import { visualizer } from 'rollup-plugin-visualizer'
 import { defineConfig, type UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -9,16 +8,7 @@ const rootDir = fileURLToPath(new URL('.', import.meta.url))
 // https://vite.dev/config/
 export default defineConfig((): UserConfig => {
   return {
-    plugins: [
-      vue(),
-      visualizer({
-        filename: 'dist/bundle-report.html',
-        template: 'sunburst',
-        gzipSize: true,
-        brotliSize: true,
-        sourcemap: false,
-      }),
-    ],
+    plugins: [vue()],
     resolve: {
       alias: {
         '@': path.resolve(rootDir, 'src'),
